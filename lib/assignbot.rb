43bcbcd7f61c1c253a62cccfc7a03475c785d78e
Adam Pahlevi <adam.pahlevi@gmail.com>
2015-10-19 05:37:53 +0700

    glue

--- lib/assignbot.rb
+++ lib/assignbot.rb
@@ -1,5 +1,48 @@
 require "assignbot/version"
+require "assignbot/core"
+
+require "assignbot/dsl/assigner_dsl"
+
+require "assignbot/exceptions/unmasked_error"
+require "assignbot/exceptions/dsl_error"
+
+require "assignbot/foundations/assignable"
+require "assignbot/foundations/assigner"
+require "assignbot/foundations/variable"
 
 module Assignbot
-  # Your code goes here...
+  def self.included(base)
+    base.extend(Assignbot::ClassDsl)
+  end
+
+  def assignbot_assign(assigner_name, hash)
+    assignable = Assignbot::Core.get_assignable(self.class)
+    assigner = assignable.get_assigner(assigner_name)
+    assigner.variables.each do |target_variable, variable_ic|
+      value = hash[variable_ic.source_variable.to_s] || hash[variable_ic.source_variable.to_sym]
+      self.send(variable_ic.receptor, value)
+    end
+  end
+  private :assignbot_assign
+
+  def assign(hash)
+    assignbot_assign(:default, hash)
+  end
+
+  def method_missing(name, *args, &block)
+    if name =~ /assign_/i
+      assigner_name = name.to_s.gsub(/^assign_/i, '')
+      assignbot_assign(assigner_name, args[0])
+    else
+      super(name, *args, &block)
+    end
+  end
+
+  module ClassDsl
+    module_function
+    def assigner(&block)
+      assigner_dsl = AssignerDsl.new(self)
+      assigner_dsl.instance_eval(&block)
+    end
+  end
 end
